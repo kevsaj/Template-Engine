f25ace6d0cb26aeecaff714a05581a76b354a964
Kevin Sajan <kevsaj@gmail.com>
2021-01-03 16:21:36 -0500

    render function

--- app.js
+++ app.js
@@ -15,57 +15,79 @@ let teamBuild = [];
 
 const addTeamMember = () => {
     inquirer.prompt(questions)
-    .then(function (ask) {
-        if (ask.Role === 'Manager') {
-            inquirer.prompt([
-                        {
-                            type: 'input',
-                            message: 'Enter your office number',
-                            name: 'officeNum'
-                        }])
-                        .then(function(next) {
-                            console.log(`Welcome ${ask.Name} in Office ${next.officeNum}`);
-                            teamBuild.push(new Manager(
-                                ask.Name, 
-                                ask.Id, 
-                                ask.Email, 
-                                next.officeNum));
-                        });   
-        } else if (ask.Role === 'Engineer') {
-            inquirer.prompt([
-                        {
-                            type: 'input',
-                            message: 'Enter your GitHub username',
-                            name: 'github'
-                        }]).then(function(next) {
-                            console.log(`Welcome ${ask.Name} on Github as ${next.github}`);
-                            teamBuild.push(new Engineer(
-                                ask.Name, 
-                                ask.Id, 
-                                ask.Email, 
-                                next.github));
-                        })
-        } else if (ask.Role === 'Intern') {
-            inquirer.prompt([
-                        {
-                            type: 'input',
-                            message: 'Enter your school',
-                            name: 'school'
-                        }])
-                        .then(function(next) {
-                            console.log(`Welcome ${ask.Name} from ${next.school}`);
-                            teamBuild.push(new Intern(
-                                ask.Name, 
-                                ask.Id, 
-                                ask.Email, 
-                                next.school));
-                        });
-        } else {
-            return 'Invalid input yo';
-        }
-    })
+        .then(function (ask) {
+            if (ask.Role === 'Manager') {
+                inquirer.prompt([{
+                        type: 'input',
+                        message: 'Enter your office number',
+                        name: 'officeNum'
+                    }])
+                    .then(function (next) {
+                        console.log(`Welcome ${ask.Name} in Office ${next.officeNum}`);
+                        teamBuild.push(new Manager(
+                            ask.Name,
+                            ask.Id,
+                            ask.Email,
+                            next.officeNum));
+                            final();
+                    });
+            } else if (ask.Role === 'Engineer') {
+                inquirer.prompt([{
+                    type: 'input',
+                    message: 'Enter your GitHub username',
+                    name: 'github'
+                }]).then(function (next) {
+                    console.log(`Welcome ${ask.Name} on Github as ${next.github}`);
+                    teamBuild.push(new Engineer(
+                        ask.Name,
+                        ask.Id,
+                        ask.Email,
+                        next.github));
+                        final();
+                })
+            } else if (ask.Role === 'Intern') {
+                inquirer.prompt([{
+                        type: 'input',
+                        message: 'Enter your school',
+                        name: 'school'
+                    }])
+                    .then(function (next) {
+                        console.log(`Welcome ${ask.Name} from ${next.school}`);
+                        teamBuild.push(new Intern(
+                            ask.Name,
+                            ask.Id,
+                            ask.Email,
+                            next.school));
+                            final();
+                    });
+            } else {
+                return 'Invalid input yo';
+            }
+        })
 }
 
+const final = () => {
+    inquirer.prompt([{
+        type: 'list',
+        message: 'Good to go?',
+        choices: ['Yes', 'No'],
+        name: 'last'
+    }]).then(function (next) {
+        console.log(`You have chosen ${next.last}`);
+        if(next.last === 'No') {
+            addTeamMember();
+        } else {
+            let teamrender = render(teamBuild);
+            fs.writeFile("index.html", render(teamBuild), function(res, err) {
+                if (err) {
+                    throw err;
+                } else {
+                    return console.log('Done yo!');
+                }
+            });
+        }
+    });
+}
 
 // function call to initialize program
 addTeamMember();
